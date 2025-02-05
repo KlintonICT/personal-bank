6190bf4efe8ace6710287c51aaa94bd9d045f94c
KlintonICT <chhunklinton91@gmail.com>
2025-02-05 11:44:28 +0700

    chore: add alias path to vite config

--- bank-app/vite.config.ts
+++ bank-app/vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite';
+import alias from '@rollup/plugin-alias';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+const projectRootDir = resolve(__dirname);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), alias({ entries: [{ find: '@', replacement: resolve(projectRootDir, 'src') }] })],
   server: {
     port: 3000,
   },
